df4f0c8ac74b80111836f5bf659ee6e8657c58be
Oscar Padilla <padillaoscara@gmail.com>
2019-01-14 17:50:45 -0500

    fixed exception handling

--- letterboxd-scraper.py
+++ letterboxd-scraper.py
@@ -1,62 +1,64 @@
 #!/bin/python
 
-from requests import get
-from requests.exceptions import RequestException 
-from contextlib import closing
 from bs4 import BeautifulSoup
-from selenium import webdriver                                      #launch/initialize a browser
-from selenium.webdriver.common.by import By                         #to search for things using specific params
-from selenium.webdriver.support.ui import WebDriverWait             #for waiting for a page load
-from selenium.webdriver.support import expected_conditions as EC    #specify what you're looking for on specific page in order to determine that the webpage has loaded
-from selenium.common.exceptions import TimeoutException, \
-    NoSuchElementException, WebDriverException                      #handling a timeout situation
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 import re
 
 
 class LetterboxdScraper:
 
+    URL_WATCHLIST = "https://letterboxd.com/{}/watchlist/"
+    URL_DIARY = 'https://letterboxd.com/{}/films/diary/'
+    # URL_POP_FILMS = 'https://letterboxd.com/films/'
+    URL_POP_FILMS = 'http://oapadilla.com/'
+
     def __init__(self, dp):
         self.driver_path = dp
+        self.driver = None
 
-    # Start the web browser/driver
     def open_browser(self):
+        """
+        Starts the web browser/driver in icognito mode.
+        """
         print('Starting driver...')
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--incognito")
         self.driver = webdriver.Chrome(executable_path=self.driver_path, chrome_options=options)
 
-    # Close the web browser/driver
     def close_browser(self):
+        """
+        Closes the web browser/driver.
+        """
         self.driver.quit()
         print("Driver closed.")
 
-    # Gets inner HTML of browser page
     def get_html(self, url):
         """
-        Navigates driver to link, scrolls to bottom of page, and extracts the HTML with JS incl.
-        :return: string
+        Navigates driver to link, interacts with page, and extracts the HTML with JS included.
+        :return: String
         """
         self.driver.get(url)
-        sleep(3)
+        sleep(0.5)
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
-        sleep(5)
+        sleep(3)
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        sleep(5)
+        sleep(3)
         inner_html = self.driver.execute_script("return document.body.innerHTML")
         return inner_html
 
-    def get_watchlist_films(self, url):
+    def get_watchlist_films(self, username):
         """
         Downloads watchlist page with url, and parses film titles.
-        :return: string array
+        :return: Array of Dictionaries
         """
+        url = self.URL_WATCHLIST.format(username)
         inner_html = self.get_html(url)
+        watchlist = []
 
-        if inner_html is not None:
-            watchlist = []
-
+        try:
             soup = BeautifulSoup(inner_html, 'html.parser')
             films = soup.find_all('li', {'class': 'poster-container'})
 
@@ -65,20 +67,21 @@ class LetterboxdScraper:
                 film_year = film.div['data-film-release-year']
 
                 watchlist.append({"title": film_title, "year": film_year})
+        except NoSuchElementException as e:
+            print(e)
 
-            return watchlist
-        raise Exception('Error retrieving contents at {}'.format("<url>"))
+        return watchlist
 
-    def get_recent_diary_entries(self, url):
+    def get_recent_diary_entries(self, username):
         """
         Downloads diary entry page with url, and parses film titles, years, and ratings.
-        :return: array of dictionaries
+        :return: Array of Dictionaries
         """
+        url = self.URL_DIARY.format(username)
         inner_html = self.get_html(url)
+        diary = []
 
-        if inner_html is not None:
-            diary = []
-
+        try:
             soup = BeautifulSoup(inner_html, 'html.parser')
             entries = soup.find_all('tr', {'class': 'diary-entry-row'})
 
@@ -89,21 +92,21 @@ class LetterboxdScraper:
                 entry_rating = entry.find('td', {'class': 'td-rating rating-green'}).div.span.meta['content']
 
                 diary.append({"title": entry_title, "year": entry_year, "rating": entry_rating})
+        except NoSuchElementException as e:
+            print(e)
 
-            return diary
-        raise Exception('Error retrieving contents at {}'.format(url))
+        return diary
 
-    def get_popular_films(self, url):
+    def get_popular_films(self):
         """
         Downloads popular films of the week with url, parses film ranks, titles, years, number of watches,
         and number of likes.
-        :return: array of dictionaries
+        :return: Array of Dictionaries
         """
-        inner_html = self.get_html(url)
+        inner_html = self.get_html(self.URL_POP_FILMS)
+        popular = []
 
-        if inner_html is not None:
-            popular = []
-
+        try:
             soup = BeautifulSoup(inner_html, 'html.parser')
             films = soup.find_all('li', {'class': 'listitem'})
             num_ranks = 8
@@ -126,107 +129,20 @@ class LetterboxdScraper:
 
                 popular.append({"rank": film_rank, "title": film_title, "year": film_year,
                                 "watches": watches, "likes": likes})
+        except NoSuchElementException as e:
+            print(e)
 
-            return popular
-        raise Exception('Error retrieving contents at {}'.format(url))
+        return popular
 
 
 if __name__ == '__main__':
-    url_watchlist = "https://letterboxd.com/narrowlightbulb/watchlist/"
-    url_diary = 'https://letterboxd.com/narrowlightbulb/films/diary/'
-    url_pop_films = 'https://letterboxd.com/films/'
 
+    user = "narrowlightbulb"
     driver_path = r'C:\Users\Oscar\AppData\Local\Google\Chrome\chromedriver.exe'
 
     lb = LetterboxdScraper(driver_path)
     lb.open_browser()
-    #lb.get_watchlist_films(url_watchlist)
-    #lb.get_recent_diary_entries(url_diary)
-    lb.get_popular_films(url_pop_films)
+    #print(lb.get_watchlist_films(user))
+    #print(lb.get_recent_diary_entries(user))
+    #print(lb.get_popular_films())
     lb.close_browser()
-
-
-
-
-# def simple_get_html(url):
-#     """
-#     HTTP GET request
-#     """
-#     try:
-#         with closing(get(url, stream=True)) as res:
-#             if is_good_response(res):
-#                 return res.content
-#             else:
-#                 return None
-#
-#     except RequestException as e:
-#         log_error('Error during requests to {0} : {1}'.format(url, str(e)))
-#         return None
-#
-#
-# def is_good_response(res):
-#     """
-#     Returns True if response is HTML
-#     """
-#     content_type = res.headers['Content-Type'].lower()
-#     return (res.status_code == 200
-#             and content_type is not None
-#             and content_type.find('html') > -1)
-#
-#
-# def log_error(e):
-#     """
-#     Prints error logs
-#     """
-#     print(e)
-#
-#
-# def get_names():
-#     """
-#     Downloads the page where the list of mathematicians is found
-#     and returns a list of strings, one per mathematician
-#     """
-#     url = 'http://www.fabpedigree.com/james/mathmen.htm'
-#     response = simple_get(url)
-#
-#     if response is not None:
-#         html = BeautifulSoup(response, 'html.parser')
-#         names = set()
-#         for li in html.select('li'):
-#             for name in li.text.split('\n'):
-#                 if len(name) > 0:
-#                     names.add(name.strip())
-#         return list(names)
-#
-#     # Raise an exception if we failed to get any data from the url
-#     raise Exception('Error retrieving contents at {}'.format(url))
-#
-#
-# def get_top_films_of_the_week():
-#     """
-#     Downloads Letterboxd weekly top movies.
-#     :return: List of strings
-#     """
-#     url = 'https://letterboxd.com/films/popular/this/week/'
-#     response = simple_get_html(url)
-#
-#     if response is not None:
-#         soup = BeautifulSoup(response, 'html.parser')
-#         movies = set()
-#
-#         return list(soup.children)
-#
-#     raise Exception('Error retrieving contents at {}'.format(url))
-#
-#
-# def get_title_by_class_name(class_name):
-#     element_list = []
-#     try:
-#         all_elements = driver.find_elements_by_class_name(class_name)
-#         element_list = [x.text for x in all_elements if len(x.text) > 0]
-#     except (NoSuchElementException, WebDriverException) as e:
-#         print(e)
-#     return element_list
-
-
-
