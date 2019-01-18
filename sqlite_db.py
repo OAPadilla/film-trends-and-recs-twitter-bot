0df11fa32ac6d1843d28004f3c764016a3c06141
Oscar Padilla <padillaoscara@gmail.com>
2019-01-18 02:35:50 -0500

    Added previous rank of film

--- sqlite_db.py
+++ sqlite_db.py
@@ -43,6 +43,8 @@ def db_select_prev_rank(conn, task):
              ORDER BY date DESC'''
     c = conn.cursor()
     c.execute(sql, task)
+    if c.fetchone() is None:
+        return 0
     prev_rank = c.fetchone()[0]
     return prev_rank
 
--- twitter_bot.py
+++ twitter_bot.py
@@ -6,19 +6,19 @@ from letterboxd_scraper import *
 
 # Once a week, call scraper to scrape top 8 movies of the week
 def scrape_weekly_pop_films():
-    print("Scraping Letterboxd's Weekly Popular Films...")
+    print("Scraping...")
     lb = LetterboxdScraper()
     lb.open_browser()
-    pop = lb.get_popular_films()
+    films = lb.get_popular_films()
     lb.close_browser()
-    return pop
+    return films
 
 
 # Store scraped data
-def store_pop_films(pop):
+def store_pop_films(films):
     print("Storing scraped data...")
     conn = connect_db()
-    for film in pop:
+    for film in films:
         task = tuple((film['rank'], film['title'], film['year'], film['watches'], film['likes']))
         db_insert_weekly_top(conn, task)
     conn.close()
@@ -26,11 +26,26 @@ def store_pop_films(pop):
 
 # Visualize/format that data
 def format_pop_films():
-    # get weekly top films
-    db_select_weekly_top()
-    # get prev ranks for each row
+    # get weekly top films and prev ranks
+    conn = connect_db()
+    films = db_select_weekly_top(conn)
+    for f in range(len(films)):
+        # append prev_rank to films tuples
+        prev_rank = db_select_prev_rank(conn, (films[f][1], films[f][2]))
+        films[f] = (films[f][0], films[f][1], films[f][2], films[f][3], films[f][4], films[f][5], prev_rank)
+    print(films)
 
-# Tweet it out
+
+# Tweets Weekly Popular Films
+def tweet_weekly_pop_films():
+    # Scrape Letterboxd
+    films_of_the_week = scrape_weekly_pop_films()
+    # Store scraped data
+    store_pop_films(films_of_the_week)
+    # Visualize/format data
+    format_pop_films()
+    # Tweet out
+    print("Tweeting...")
 
 
 if __name__ == '__main__':
@@ -43,7 +58,8 @@ if __name__ == '__main__':
     print(user.name)
     #api.update_status('test')
 
-    films_of_the_week = scrape_weekly_pop_films()
-    print(films_of_the_week)
-    store_pop_films(films_of_the_week)
+    #tweet_weekly_pop_films()
+
+    format_pop_films()
+
 
