3c5c8e884b0a212aaf61f08737add486512231d1
Oscar Padilla <padillaoscara@gmail.com>
2019-03-10 07:20:27 -0400

    tmdb metadata profile redundancy removed

--- twitter_bot.py
+++ twitter_bot.py
@@ -101,7 +101,7 @@ def tweet_weekly_pop_films():
     print(status.id)
 
 
-def tweet_recommended_films(screen_name, status_id, letterboxd_name):
+def tweet_recommended_films(screen_name, status_id, letterboxd_name, md):
     """
     Tweet replies with recommended films based on tweets provided Letterboxd username
     :return: Integer of status id
@@ -113,11 +113,11 @@ def tweet_recommended_films(screen_name, status_id, letterboxd_name):
     # diary = [{'title': 'Vice', 'year': '2018', 'rating': '6'}, {'title': 'Magnolia', 'year': '1999', 'rating': '10'}, {'title': 'Roma', 'year': '2018', 'rating': '9'}, {'title': 'Children of Men', 'year': '2006', 'rating': '9'}, {'title': 'Black Mirror: Bandersnatch', 'year': '2018', 'rating': '5'}, {'title': 'Take Shelter', 'year': '2011', 'rating': '7'}, {'title': 'The Ballad of Buster Scruggs', 'year': '2018', 'rating': '7'}, {'title': 'Incredibles 2', 'year': '2018', 'rating': '6'}, {'title': 'Force Majeure', 'year': '2014', 'rating': '7'}, {'title': 'Sorry to Bother You', 'year': '2018', 'rating': '8'}, {'title': 'BlacKkKlansman', 'year': '2018', 'rating': '6'}, {'title': 'Hereditary', 'year': '2018', 'rating': '8'}, {'title': 'It Follows', 'year': '2014', 'rating': '7'}, {'title': 'Black Swan', 'year': '2010', 'rating': '9'}, {'title': 'Mandy', 'year': '2018', 'rating': '7'}, {'title': 'It', 'year': '2017', 'rating': '6'}, {'title': 'What We Do in the Shadows', 'year': '2014', 'rating': '8'}, {'title': 'Mamma Mia! Here We Go Again', 'year': '2018', 'rating': '4'}, {'title': 'Isle of Dogs', 'year': '2018', 'rating': '7'}, {'title': 'Thoroughbreds', 'year': '2017', 'rating': '7'}, {'title': 'The Disaster Artist', 'year': '2017', 'rating': '6'}, {'title': 'Solaris', 'year': '1972', 'rating': '0'}, {'title': 'Fallen Angels', 'year': '1995', 'rating': '8'}, {'title': 'The Master', 'year': '2012', 'rating': '7'}, {'title': 'Suspiria', 'year': '1977', 'rating': '9'}, {'title': 'Welcome to Leith', 'year': '2015', 'rating': '6'}, {'title': 'Death Note', 'year': '2017', 'rating': '3'}, {'title': 'There Will Be Blood', 'year': '2007', 'rating': '10'}, {'title': 'Cube 2: Hypercube', 'year': '2002', 'rating': '2'}, {'title': 'Cube', 'year': '1997', 'rating': '4'}, {'title': "One Flew Over the Cuckoo's Nest", 'year': '1975', 'rating': '9'}, {'title': 'Mulholland Drive', 'year': '2001', 'rating': '0'}, {'title': 'Star Wars: The Last Jedi', 'year': '2017', 'rating': '5'}, {'title': 'Coherence', 'year': '2013', 'rating': '6'}, {'title': 'Avengers: Infinity War', 'year': '2018', 'rating': '7'}, {'title': 'The One I Love', 'year': '2014', 'rating': '7'}, {'title': 'Annihilation', 'year': '2018', 'rating': '7'}, {'title': "The Devil's Backbone", 'year': '2001', 'rating': '9'}, {'title': 'Coco', 'year': '2017', 'rating': '7'}, {'title': 'Office Space', 'year': '1999', 'rating': '8'}, {'title': 'The Cloverfield Paradox', 'year': '2018', 'rating': '3'}, {'title': 'Good Time', 'year': '2017', 'rating': '8'}, {'title': 'The Shape of Water', 'year': '2017', 'rating': '8'}, {'title': 'Three Billboards Outside Ebbing, Missouri', 'year': '2017', 'rating': '7'}, {'title': 'The Killing of a Sacred Deer', 'year': '2017', 'rating': '7'}, {'title': 'The Death of Stalin', 'year': '2017', 'rating': '7'}, {'title': 'The Florida Project', 'year': '2017', 'rating': '9'}, {'title': 'Once', 'year': '2007', 'rating': '7'}, {'title': 'Blade Runner 2049', 'year': '2017', 'rating': '10'}, {'title': 'Blade Runner', 'year': '1982', 'rating': '9'}]
     print(diary)
     # Create user profile and TMDb dataset metadata dataframes
-    print("Creating user profile and TMDb dataset dataframes...")
-    md = create_metadata_dataframe()
+    print("Creating user profile dataframe...")
+    # md = create_metadata_dataframe()
     up = create_user_profile(diary)
     # Make user word soups
-    md['soup'] = md.apply(make_soup, axis=1)
+    # md['soup'] = md.apply(make_soup, axis=1)
     up['soup'] = up.apply(make_soup, axis=1)
     # Find similarity matrix
     sim_matrix = make_sim_matrix(md['soup'], up['soup'])
@@ -143,7 +143,7 @@ class MyStreamListener(tweepy.StreamListener):
         words = status.text.split(' ')
         if len(words) > 1:
             lb_username = words[1]
-            tweet_recommended_films(sn, status.id, lb_username)
+            tweet_recommended_films(sn, status.id, lb_username, tmdb_md)
         else:
             api.update_status("@" + sn + " Please provide your Letterboxd username!", in_reply_to_status_id=status.id)
 
@@ -161,6 +161,12 @@ if __name__ == '__main__':
 
     # Tweet Weekly Popular Films on Letterboxd every Saturday, 5 PM
     schedule.every().saturday.at("17:00").do(tweet_weekly_pop_films)
+
+    # Create TMDb dataset metadata dataframe and word soup, needed for recommender system
+    print("Creating TMDb dataset dataframe and word soup...")
+    tmdb_md = create_metadata_dataframe()
+    tmdb_md['soup'] = tmdb_md.apply(make_soup, axis=1)
+    print("Done. Stream Listener active...")
 
     while True:
         schedule.run_pending()
